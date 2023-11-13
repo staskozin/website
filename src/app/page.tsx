eaddbf688dec3c7117f5b0d79a4aa3c244bd245f
Stanislav Kozin <stas@staskozin.ru>
2023-11-13 19:47:31 +0300

    Доработал шапку и подвал

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Header from '@/components/Header'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Станислав Козин. Веб-разработчик и дизайнер',
+  description: 'Сайт-портфолио фронтенд-разработчика',
+}
+
 export default function Home() {
   return (
-    <>Контент</>
+    <>
+      <Header title='Веб-разработчик и&nbsp;дизайнер' />
+      <span>Контент</span>
+    </>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,21 @@ import s from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={s.footer}>
-      Станислав Козин, 2016–н.в.
+      <span>Станислав Козин, 2016–н.в.</span>
+      <ul className={s.contacts}>
+        <li>
+          <a href="https://hh.ru/resume/d0c04ea9ff044224bc0039ed1f794536703956">Резюме</a>
+        </li>
+        <li>
+          <a href="https://github.com/staskozin">Гитхаб</a>
+        </li>
+        <li>
+          <a href="https://t.me/staskozin">Телеграм</a>
+        </li>
+        <li>
+          <a href="mailto:stas@staskozin.ru">stas@staskozin.ru</a>
+        </li>
+      </ul>
     </footer>
   )
 }
