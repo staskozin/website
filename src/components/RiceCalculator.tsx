f1b9aa77075ead6ed07e08f08d75b348852b86d9
Stanislav Kozin <stas@staskozin.ru>
2023-11-23 02:31:48 +0300

    Добавил поле ввода кол-ва риса

--- src/components/RiceCalculator.tsx
+++ src/components/RiceCalculator.tsx
@@ -4,14 +4,24 @@ import { useState } from 'react'
 
 import s from './RiceCalculator.module.css'
 import Checkbox from '@/components/UI/Checkbox'
+import TextInput from '@/components/UI/TextInput'
 
 
 export default function RiceCalculator() {
-  const [scale, setScale] = useState(true)
+  const [scale, setScale] = useState<boolean>(true)
+  const [rice, setRice] = useState<number>(450)
 
   return (
-    <form>
+    <form className={s.control}>
       <Checkbox className={s.checkbox} text='У меня есть весы' setFunction={setScale} checked={scale} />
+
+      <div className={s.text}>Я&nbsp;буду варить</div>
+      {' '}
+      <TextInput className={s.text} value={rice} setFunction={setRice} />
+      {' '}
+      <div className={s.text}>
+        грамм
+      </div>
     </form>
   )
 }
---
+++ src/components/UI/TextInput.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, SetStateAction, useEffect, useReducer, useRef } from 'react'
+
+import s from './TextInput.module.css'
+
+
+export default function RiceCalculator(props: CheckboxProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [forcedUpdate, forceUpdate] = useReducer(x => x + 1, 0)
+
+  let cursorPosition = useRef<number>(0)
+
+  useEffect(() => {
+    if (forcedUpdate > 0)
+      inputRef.current!.setSelectionRange(cursorPosition.current, cursorPosition.current)
+  }, [forcedUpdate])
+
+  return (
+    <div className={`${s.wrap} ${props.className ? props.className : ''}`}>
+      <input
+        className={s.input}
+        ref={inputRef}
+        value={props.value}
+        type="text"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        onChange={e => {
+          if (e.target.validity.valid) {
+            const value = Number(e.target.value)
+            cursorPosition.current = e.target.selectionStart!
+            props.setFunction(value > 2000000 ? 2000000 : value)
+          } else {
+            cursorPosition.current = e.target.selectionStart! - 1
+            forceUpdate()
+          }
+        }}
+        onKeyDown={e => {
+          if (e.key === 'Enter' || e.key === 'Escape')
+            inputRef.current!.blur()
+        }}
+        onBlur={() => {
+          if (!props.value)
+            props.setFunction(450)
+        }}
+      />
+      <span>{props.value}</span>
+    </div>
+  )
+}
+
+type CheckboxProps = {
+  value: number
+  setFunction: Dispatch<SetStateAction<number>>
+  className?: string
+}
