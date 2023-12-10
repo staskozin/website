dcc8bdffe21f4be30b9a95c86b3c9e405aa8f0ec
Stanislav Kozin <stas@staskozin.ru>
2023-12-10 15:09:10 +0300

    Поменял switch на if в редьюсере рецепта

--- src/components/RiceCalculator/reducer.ts
+++ src/components/RiceCalculator/reducer.ts
@@ -1,77 +1,83 @@
 export const reducer: CalcReducer = (state: CalcState, action: CalcAction): CalcState => {
   let newState: CalcState = { ...state }
-  switch (action.type) {
-    case 'changeScale':
-      newState.scale = action.payload
-      break
 
-    case 'changeRice':
-      const proportion = Math.abs(action.payload / 500)
-      newState.rice = action.payload
-      newState.ingredients = {
-        // Здесь надо вызывать функцию, которая зависит от состояния рецепта
-        // Для суши одна, для гарнира другая и т.д.
-        // И пересчитывать ингредиенты в каждом экшне
-        water: Math.round(550 * proportion),
-        vinegar: Math.round(55 * proportion),
-        sugar: Math.round(45 * proportion),
-        salt: Math.round(5 * proportion)
-      }
-      break
-
-    case 'changePurpose':
-      newState.purpose = action.payload
-      if (action.payload === 'side') {
-        newState.disabledButtons.riceType = {
-          round: false,
-          long: false,
-          parboiled: false
-        }
-        newState.disabledButtons.pot = {
-          pot: false,
-          multi: false,
-          pan: false
-        }
-      } else if (action.payload === 'porridge') {
-        newState.riceType = 'round'
-        newState.pot = state.pot !== 'pan' ? state.pot : 'pot'
-        newState.disabledButtons.riceType = {
-          round: false,
-          long: true,
-          parboiled: true
-        }
-        newState.disabledButtons.pot = {
-          pot: false,
-          multi: false,
-          pan: true
-        }
-      } else {
-        newState.riceType = 'round'
-        newState.pot = state.pot !== 'pan' ? state.pot : 'pot'
-        newState.disabledButtons.riceType = {
-          round: false,
-          long: true,
-          parboiled: true
-        }
-        newState.disabledButtons.pot = {
-          pot: false,
-          multi: false,
-          pan: true
-        }
-      }
-      break
-
-    case 'changeRiceType':
-      newState.riceType = action.payload
-      break
-
-    case 'changePot':
-      newState.pot = action.payload
-      break
-
-    default:
-      throw new Error('Неправильный action')
+  if (action.type === 'changeScale') {
+    newState.scale = action.payload
   }
+
+  else if (action.type === 'changeRiceType') {
+    newState.riceType = action.payload
+  }
+
+  else if (action.type === 'changePot') {
+    newState.pot = action.payload
+  }
+
+  else if (action.type === 'changeRice') {
+    const proportion = Math.abs(action.payload / 500)
+    newState.rice = action.payload
+    newState.ingredients = {
+      // Здесь надо вызывать функцию, которая зависит от состояния рецепта
+      // Для суши одна, для гарнира другая и т.д.
+      // И пересчитывать ингредиенты в каждом экшне
+      water: Math.round(550 * proportion),
+      vinegar: Math.round(55 * proportion),
+      sugar: Math.round(45 * proportion),
+      salt: Math.round(5 * proportion)
+    }
+  }
+
+  else if (action.type === 'changePurpose') {
+    newState.purpose = action.payload
+
+    if (action.payload === 'side') {
+      newState.disabledButtons.riceType = {
+        round: false,
+        long: false,
+        parboiled: false
+      }
+      newState.disabledButtons.pot = {
+        pot: false,
+        multi: false,
+        pan: false
+      }
+    }
+
+    else if (action.payload === 'porridge') {
+      newState.riceType = 'round'
+      newState.pot = state.pot !== 'pan' ? state.pot : 'pot'
+      newState.disabledButtons.riceType = {
+        round: false,
+        long: true,
+        parboiled: true
+      }
+      newState.disabledButtons.pot = {
+        pot: false,
+        multi: false,
+        pan: true
+      }
+    }
+
+    else {
+      newState.riceType = 'round'
+      newState.pot = state.pot !== 'pan' ? state.pot : 'pot'
+      newState.disabledButtons.riceType = {
+        round: false,
+        long: true,
+        parboiled: true
+      }
+      newState.disabledButtons.pot = {
+        pot: false,
+        multi: false,
+        pan: true
+      }
+    }
+  }
+
+  else {
+    throw new Error('Неправильный action')
+  }
+
   return newState
 }
 
