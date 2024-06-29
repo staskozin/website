cda3df8d44beca645d1df1cbb094d7f7396b136a
Stanislav Kozin <stas@staskozin.ru>
2024-06-30 02:30:06 +0300

    Исправил причину тряски картинок с перспективой

--- src/components/Image/ScrollableImage.tsx
+++ src/components/Image/ScrollableImage.tsx
@@ -6,14 +6,13 @@ import s from './ScrollableImage.module.css'
 
 export default function ScrollableImage(props: ScrollableImageProps) {
   const [currentImage, setCurrentImage] = useState(props.src[0])
-  const controlRef = useRef<HTMLDivElement>(null)
   const imageRef = useRef<HTMLImageElement>(null)
   const wrapRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
+    wrapRef.current!.addEventListener('mousemove', (e) => handleMove(e.clientX, e.clientY, 10))
+    wrapRef.current!.addEventListener('touchmove', (e) => handleMove(e.changedTouches[0].clientX, e.changedTouches[0].clientY, 7))
     if (props.perspective) {
-      wrapRef.current!.addEventListener('mousemove', handleMove)
-      wrapRef.current!.addEventListener('touchmove', handleMobileMove)
       wrapRef.current!.addEventListener('mouseout', resetPerspective)
       wrapRef.current!.addEventListener('touchend', resetPerspective)
       wrapRef.current!.addEventListener('touchstart', setShadow)
@@ -21,28 +20,33 @@ export default function ScrollableImage(props: ScrollableImageProps) {
     }
   }, [props.perspective])
 
-  function handleMove(e: MouseEvent) {
+  function handleMove(clientX: number, clientY: number, rotationModifier: number) {
     const n = wrapRef.current!.getBoundingClientRect()
-    const xVal = e.clientX - n.x
-    const yVal = e.clientY - n.y
-    const yRotation = 7 * ((xVal - imageRef.current!.clientWidth / 2) / imageRef.current!.clientWidth)
-    const xRotation = -7 * ((yVal - imageRef.current!.clientHeight / 2) / imageRef.current!.clientHeight)
-    const string = 'perspective(' + imageRef.current!.clientWidth + 'px) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg) scale(1.02)'
-    requestAnimationFrame(() => {
-      imageRef.current!.style.transform = string
-    })
+    const xVal = clientX - n.x
+    if (props.perspective) {
+      const yVal = clientY - n.y
+      const yRotation = rotationModifier * ((xVal - imageRef.current!.clientWidth / 2) / imageRef.current!.clientWidth)
+      const xRotation = -rotationModifier * ((yVal - imageRef.current!.clientHeight / 2) / imageRef.current!.clientHeight)
+      const styles = 'perspective(' + imageRef.current!.clientWidth + 'px) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg) scale(1.02)'
+      requestAnimationFrame(() => {
+        imageRef.current!.style.transform = styles
+        selectImage(xVal)
+      })
+    } else {
+      requestAnimationFrame(() => {
+        selectImage(xVal)
+      })
+    }
   }
 
-  function handleMobileMove(e: TouchEvent) {
-    const n = wrapRef.current!.getBoundingClientRect()
-    const xVal = e.changedTouches[0].clientX - n.x
-    const yVal = e.changedTouches[0].clientY - n.y
-    const yRotation = 10 * ((xVal - imageRef.current!.clientWidth / 2) / imageRef.current!.clientWidth)
-    const xRotation = -10 * ((yVal - imageRef.current!.clientHeight / 2) / imageRef.current!.clientHeight)
-    const string = 'perspective(' + imageRef.current!.clientWidth + 'px) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg) scale(1.02)'
-    requestAnimationFrame(() => {
-      imageRef.current!.style.transform = string
-    })
+  function selectImage(xVal: number) {
+    const selected = Math.trunc(xVal / (imageRef.current!.clientWidth / props.src.length))
+    if (selected > props.src.length - 1)
+      setCurrentImage(props.src[props.src.length - 1])
+    else if (selected < 0)
+      setCurrentImage(props.src[0])
+    else
+      setCurrentImage(props.src[selected])
   }
 
   function resetPerspective() {
@@ -54,29 +58,6 @@ export default function ScrollableImage(props: ScrollableImageProps) {
     imageRef.current!.style.boxShadow = 'rgba(50, 50, 93, 0.3) 0px 13px 27px -5px, rgba(0, 0, 0, 0.35) 0px 8px 16px -8px'
   }
 
-  function createSegments(images: Array<string>): Array<JSX.Element> {
-    return images.map(i => {
-      return <div
-        className={s.segment}
-        key={i}
-        onMouseEnter={() => {
-          setCurrentImage(i)
-        }}
-        onTouchMove={e => {
-          const coords = e.changedTouches[0].clientX - controlRef.current!.getBoundingClientRect().x
-          const width = imageRef.current!.getBoundingClientRect().width / props.src.length
-          const selected = Math.floor(coords / width)
-          if (selected > props.src.length - 1)
-            setCurrentImage(props.src[props.src.length - 1])
-          else if (selected < 0)
-            setCurrentImage(props.src[0])
-          else
-            setCurrentImage(props.src[selected])
-        }}
-      />
-    })
-  }
-
   let className = ''
   if (props.perspective)
     className += `${s.perspective} `
@@ -84,10 +65,7 @@ export default function ScrollableImage(props: ScrollableImageProps) {
     className += `${s.rounded} `
 
   return (
-    <div ref={wrapRef}>
-      <div ref={controlRef} className={s.control}>
-        {createSegments(props.src)}
-      </div>
+    <div ref={wrapRef} >
       <Image className={className} ref={imageRef} src={currentImage} alt={props.alt} fill sizes='550px' />
     </div>
   )
